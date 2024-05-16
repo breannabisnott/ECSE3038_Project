e4f8074848903853c8b2994cfd5b7d7ea3f3b8be
breannabisnott <breanna.bisnott@gmail.com>
2024-05-16 00:42:03 -0500

    first commit

---
+++ embeded/include/env.h
@@ -0,0 +1,6 @@
+const char* ssid = "LIVING IRVIN HALL GUEST";
+const char* password = "Resident2020@138sl";
+String endpoint = "http://127.0.0.1:8000/sensorData";
+const char* api_key = "Clarence#3507";
+#define ONE_WIRE_BUS 4
+#define LED 13
