9f2d8dc4c0d8a89ab0f688b7a91b760d4c74f4b4
Breanna <158131996+breannabisnott@users.noreply.github.com>
2024-05-17 22:20:31 -0500

    Delete embeded/include/env.h

--- embeded/include/env.h
+++
@@ -1,9 +0,0 @@
-const char* ssid = "LIVING IRVIN HALL GUEST";
-const char* password = "Resident2020@138sl";
-
-#define ONE_WIRE_BUS 4
-#define pirPin 15
-#define fan 23
-#define light 12
-
-String endpoint = "https://ecse3038-project-bb.onrender.com/sensorData";
